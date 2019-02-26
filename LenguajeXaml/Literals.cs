5326fd711fc79dbfaeddc85681bad7c48f123ef1
Brandon Vite <bramsel.vite@gmail.com>
2019-02-25 20:38:49 -0600

    GridColor

---
+++ LenguajeXaml/Literals.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace LenguajeXaml
+{
+    public class Literals
+    {
+        public static string Message = "Hola a todos desde Platzi";
+    }
+}
---
+++ LenguajeXaml/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LenguajeXaml
+{
+    public partial class MainPage : ContentPage
+    {
+        Random rnd = new Random();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTapGestureRecognizerTapped(object sender, EventArgs e)
+        {
+            Color rndColor = Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+
+            var boxColor = sender as BoxView;
+            if (boxColor.Color == Color.White)
+            {
+                boxColor.Color = rndColor;
+            }
+            else
+            {
+                boxColor.Color = Color.White;
+            }
+        }
+    }
+}
